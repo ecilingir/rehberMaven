643b757a48bf8cab28013b33930cd8c6853ca671
BEM <BEM@10.0.0.214>
2015-08-27 10:10:33 +0300

    kisi ekle ve listele çalıştırıldı

---
+++ src/main/java/tr/gov/ptt/rehbermaven/bean/KisiBean.java
@@ -0,0 +1,42 @@
+
+package tr.gov.ptt.rehbermaven.bean;
+
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import tr.gov.ptt.rehbermaven.entity.Kisi;
+import tr.gov.ptt.rehbermaven.service.KisiService;
+import tr.gov.ptt.rehbermaven.util.JSFUtil;
+
+@ManagedBean
+@RequestScoped
+public class KisiBean {
+    
+    private Kisi kisi = new Kisi();
+    @EJB
+    private KisiService kisiService;
+
+    public KisiBean() {
+    }
+
+    public Kisi getKisi() {
+        return kisi;
+    }
+
+    public void setKisi(Kisi kisi) {
+        this.kisi = kisi;
+    }
+    public String  Ekle()
+    {
+       kisiService.ekle(kisi);
+        JSFUtil.mesajGoster("Kişi Eklendi", kisi.getAd()+" "+kisi.getSoyad()+" eklendi.");
+       return "kisiListele.xhtml?faces-redirect=true";
+    }
+    
+    public List<Kisi> getKisiListe()
+    {
+      return kisiService.kisileriGetir();
+    }
+    
+}
---
+++ src/main/java/tr/gov/ptt/rehbermaven/facade/KisiFacade.java
@@ -0,0 +1,25 @@
+
+package tr.gov.ptt.rehbermaven.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import tr.gov.ptt.rehbermaven.entity.Kisi;
+
+
+@Stateless
+public class KisiFacade extends AbstractFacade<Kisi> {
+    @PersistenceContext(unitName = "RehberMavenPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public KisiFacade() {
+        super(Kisi.class);
+    }
+    
+    
+}
